336bbe74dd5a577e8c1eceb1271879dc59178f87
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2024-12-06 22:09:15 +0100

    Descrizione delle modifiche

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 });
 
 // Endpoint API
-const rugbyDayEndDate = new Date('2024-12-06T23:59:59');
+const rugbyDayEndDate = new Date('2024-12-06T19:30:59');
 app.get('/api/rugbyDayEnd', (req, res) => {
   res.json({ endDate: rugbyDayEndDate }); // Restituisce la data come JSON
 });
