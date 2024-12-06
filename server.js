56dc2d9fbc3a6f8be46e9c6ccb377c7db19e3411
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2024-12-06 07:46:37 +0100

    Fix server binding to 0.0.0.0

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get('/', (req, res) => {
 });
 
 // Avvia il server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend running at http://localhost:${PORT}`);
 });
