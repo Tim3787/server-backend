ca3fdaaae7d7af5fac3a8782d0e2bbba0706f63d
Tim3787 <115894589+Tim3787@users.noreply.github.com>
2024-12-06 07:38:22 +0100

    Fix middleware order and ensure PORT usage

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-// Leggi le variabili d'ambiente
-const PORT = process.env.PORT || 3000; 
-const DATABASE_URL = process.env.DATABASE_URL;
 const helmet = require('helmet');
+const app = express();
 
+// Leggi le variabili d'ambiente
+const PORT = process.env.PORT || 3000;
+
+// Usa Helmet per configurare la CSP
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", "https:"], // Permetti risorse dalla stessa origine e HTTPS
+        scriptSrc: ["'self'", "'unsafe-inline'", "https:"], // Aggiungi domini di script sicuri
+        workerSrc: ["'self'", "blob:"], // Permetti blob workers
+        imgSrc: ["'self'", "data:", "https:"], // Permetti immagini inline e da HTTPS
+      },
+    },
+  })
+);
 
 // Middleware per CORS (per permettere al frontend di accedere alle API)
 app.use((req, res, next) => {
@@ -21,25 +34,12 @@ app.get('/api/rugbyDayEnd', (req, res) => {
   res.json({ endDate: rugbyDayEndDate }); // Restituisce la data come JSON
 });
 
-// Avvia il server
-app.listen(PORT, () => {
-  console.log(`Backend running at http://localhost:${PORT}`);
-});
-
+// Endpoint base
 app.get('/', (req, res) => {
   res.send('API Backend is running!');
 });
 
-// Usa Helmet per configurare la CSP
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'", "https:"], // Permetti risorse dalla stessa origine e HTTPS
-        scriptSrc: ["'self'", "'unsafe-inline'", "https:"], // Aggiungi domini di script sicuri
-        workerSrc: ["'self'", "blob:"], // Permetti blob workers
-        imgSrc: ["'self'", "data:", "https:"], // Permetti immagini inline e da HTTPS
-      },
-    },
-  })
-);
+// Avvia il server
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
+});
